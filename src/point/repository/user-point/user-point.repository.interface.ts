9ef87123959e7e688f43c33daabdd7b8fbb15b90
sangyeopchae <sy.chae@teamsparta.co>
2024-06-16 21:13:14 +0900

    refactor: 도메인 클래스 자체를 레포지토리계층에서 전달 받도록 수정

--- src/point/repository/user-point/user-point.repository.interface.ts
+++ src/point/repository/user-point/user-point.repository.interface.ts
@@ -1,6 +1,7 @@
+import { UserPointDomain } from 'src/point/domain/user-point.domain';
 import { UserPoint } from 'src/point/model/point.model';
 
 export interface IUserPointRepository {
   selectById(id: number): Promise<UserPoint>;
-  insertOrUpdate(id: number, amount: number): Promise<UserPoint>;
+  insertOrUpdate(userPointDomain: UserPointDomain): Promise<UserPoint>;
 }
--- src/point/repository/user-point/user-point.repository.ts
+++ src/point/repository/user-point/user-point.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { IUserPointRepository } from './user-point.repository.interface';
 import { UserPointTable } from 'src/database/userpoint.table';
 import { UserPoint } from 'src/point/model/point.model';
+import { UserPointDomain } from 'src/point/domain/user-point.domain';
 
 export const USER_POINT_REPOSITORY_TOKEN = Symbol('IUserPointRepository');
 
@@ -12,7 +13,10 @@ export class UserPointRepository implements IUserPointRepository {
   async selectById(id: number): Promise<UserPoint> {
     return this.userPointModel.selectById(id);
   }
-  async insertOrUpdate(id: number, amount: number): Promise<UserPoint> {
-    return this.userPointModel.insertOrUpdate(id, amount);
+  async insertOrUpdate(userPointDomain: UserPointDomain): Promise<UserPoint> {
+    return this.userPointModel.insertOrUpdate(
+      userPointDomain.getId(),
+      userPointDomain.getPoint(),
+    );
   }
 }
